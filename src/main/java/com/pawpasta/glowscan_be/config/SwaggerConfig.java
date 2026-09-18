e9ae87f2f9060b3edb6069258d993df5019e4e50
PawPasta <giabaostrike2004@gmail.com>
2026-09-18 17:55:07 +0700

    feat: CORS, Swagger and FilterChain

--- src/main/java/com/pawpasta/glowscan_be/config/SwaggerConfig.java
+++ src/main/java/com/pawpasta/glowscan_be/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.pawpasta.glowscan_be.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,6 +17,13 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("GlowScan API")
                         .description("REST API documentation for the GlowScan backend.")
-                        .version("v1"));
+                        .version("v1"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .description("Paste only the access token; Swagger adds the Bearer prefix.")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
     }
 }
