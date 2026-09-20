ee3ef77fcb515aa3106cfd9ee6c5e79755eb951b
PawPasta <giabaostrike2004@gmail.com>
2026-09-20 17:27:08 +0700

    delete: remove unused file

--- src/main/java/com/pawpasta/glowscan_be/service/JwtService.java
+++
@@ -1,8 +0,0 @@
-package com.pawpasta.glowscan_be.service;
-
-import com.pawpasta.glowscan_be.modal.entity.User;
-
-public interface JwtService {
-    String generateToken(User user);
-
-}
--- src/main/java/com/pawpasta/glowscan_be/service/OpaqueTokenService.java
+++
@@ -1,7 +0,0 @@
-package com.pawpasta.glowscan_be.service;
-
-public interface OpaqueTokenService {
-
-    String generateToken();
-    String hashToken(String token);
-}
